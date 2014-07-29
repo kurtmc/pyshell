612b7889e683aa4abe94964366b943779cbacdd8
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-07-29 14:33:19 +1200

    Forwards commands to os.system

--- pysh.py
+++ pysh.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+import os
+import subprocess
 
 def main():
 	while True:
-		input("pysh> ");
+		command = input("pysh> ");
+		runCommand(command)
+def runCommand(command):
+	os.system(command)
+	#subprocess.call(command)
+	
 		
 
 if __name__ == "__main__":
