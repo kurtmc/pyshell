60dce321d0feefe534dbfdc871f508aa2d235d25
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-07-29 14:50:43 +1200

    Made the prompt look a bit better

--- pysh.py
+++ pysh.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 import os
 import subprocess
+import socket
+
+def stripByteArray(byteArray):
+	return str(byteArray[:len(byteArray)-1].decode("utf-8"))
+
+def getPrompt():
+	proc = subprocess.Popen(["whoami"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	# Get user information so that it can be displayed on command line input
+	username = stripByteArray(out)
+	proc = subprocess.Popen(["uname", "-n"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	host = socket.gethostname()#stripByteArray(out)	
+	proc = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	pwd = stripByteArray(out)
+	return username + "@" + host + ":" + pwd + "> "
 
 def main():
 	while True:
-		command = input("pysh> ");
+		command = input(getPrompt());
 		runCommand(command)
 def runCommand(command):
 	os.system(command)
