f61b034e4786132638da064b5b35d4d01844c079
Kurt McAlpine <kurtmcalpine@gmail.com>
2014-08-13 08:53:58 +1200

    Args working now

--- pysh.py
+++ pysh.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import curses
-from os import execvp
 import os
 from prompt import Prompt
 import shlex
@@ -23,64 +22,35 @@ def main():
         lexer.wordchars += '#$+-,./?@^='
         args = list(lexer)
         
-        #if (/* command line contains & */)
-        #   amper = 1;
-        #else
-        #   amper = 0;
+
         if "&" in commandLine:
             amper = True
         else:
             amper = False
-        #/* for commands not part of the shell command language */
-        #if (fork() == 0)
-        #{
+
+
         pid = os.fork()
         if pid == 0:
-
-        #   if (/* piping */)
-        #   {
             if "|" in commandLine:
-        #      pipe(fildes);
+
                 (read, write) = os.pipe()
-        #      if (fork() == 0)
-        #      {
+
                 if os.fork() == 0: # Child process
                     os.close(read)
                     write = os.fdopen(write, 'w')
-        #         /* first component of command line */
-        #         close(stdout);
-        #         dup(fildes[1]);
-        #         close(fildes[1]);
-        #         close(fildes[0]);
-        #         /* stdout now goes to pipe */
-        #         /* child process does command */
-        #         execlp(command1, command1, 0);
+
                     os.execlp(commandLine)
-        #      }
-                # Parent process
+
                 os.close(write)
                 read = os.fdopen(read)
-        #      /* second component of command line */
-        #      close(stdin);
-        #      dup(fildes[0]);
-        #      close(fildes[0]);
-        #      close(fildes[1]);
-        #      /* standard input now comes from the pipe */
-        #   }
-        #   execve(command2, command2, 0);
-            print(args[0])
-            print(args[1:])
+
+
             if len(args[1:]) == 0:
-              execvp(args[0], [''])
+              os.execvp(args[0], [''])
               #execvp(args[0], args[1:])
             else:
-              execvp(args[0], args[1:])
-        #}
-        #/* parent continues over here...
-        #* waits for child to exit if required
-        #*/
-        #if (amper == 0)
-        #   retid = wait(&status);
+              os.execvp(args[0], args)
+
         if amper == False:
             os.wait()
 
